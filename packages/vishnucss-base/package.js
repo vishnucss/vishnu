058eb8dc03906bf45f6e8d1a23de7d87340b057e
Allan Alexandre <alexandesigner@gmail.com>
2017-10-08 19:14:53 -0300

    Bumb version v0.0.1-0

--- packages/vishnucss-base/package.js
+++ packages/vishnucss-base/package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'vishnucss:base',
-	version: '1.0.0-rc.0',
+	version: '1.0.0-0',
 	summary: 'A simple and minimalist toolkit with modern css.',
 	git: 'https://github.com/vishnucss/vishnu.git',
 	documentation: 'README.md'
