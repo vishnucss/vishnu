4aad8f1f9003f0678ac4efac8a0d7108b9f31687
Allan Alexandre <alexandesigner@gmail.com>
2020-01-21 00:35:42 -0300

    Bump version 1.1.0-rc.1

--- packages/vishnucss-base/package.js
+++ packages/vishnucss-base/package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'vishnucss:base',
-	version: '1.1.0-beta.1',
+	version: '1.1.0-rc.1',
 	summary: 'Base toolkit with modern css.',
 	git: 'https://github.com/vishnucss/vishnu.git',
 	documentation: 'README.md'
--- packages/vishnucss-collapse/package.js
+++ packages/vishnucss-collapse/package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'vishnucss:collapse',
-	version: '1.1.0-beta.1',
+	version: '1.1.0-rc.1',
 	summary: 'Collapse toolkit with modern css.',
 	git: 'https://github.com/vishnucss/vishnu.git',
 	documentation: 'README.md'
