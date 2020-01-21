b94216fb618a898a2110dcc92a84f8c093e2e4c7
Allan Alexandre <alexandesigner@gmail.com>
2020-01-20 22:02:01 -0300

    feat(chips): add paths

---
+++ packages/vishnucss-chips/package.js
@@ -0,0 +1,14 @@
+Package.describe({
+	name: 'vishnucss:card',
+	version: '1.1.0-beta.1',
+	summary: 'Card toolkit with modern css.',
+	git: 'https://github.com/vishnucss/vishnu.git',
+	documentation: 'README.md'
+})
+
+Package.onUse((api) => {
+	api.versionsFrom('METEOR@1.0')
+	api.addFiles([
+		'dist/vishnu.card.css'
+	], 'client')
+})
