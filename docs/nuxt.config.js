a1600e3c73fdbf877117cf24e300ffa3c1fa9ae5
Allan Alexandre <alexandesigner@gmail.com>
2020-09-06 19:35:02 -0300

    Add target and router

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -1,6 +1,10 @@
 import theme from '@nuxt/content-theme-docs'
 
 export default theme({
+  target: 'static',
+  router: {
+    base: 'https://github.com/vishnucss/vishnu'
+  },
   loading: { color: '#8549b9' },
   liveEdit: false
 })
