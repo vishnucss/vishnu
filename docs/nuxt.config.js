10de66412a44f2be578893d1ce0e7018fff4b2fe
Allan Alexandre <alexandesigner@gmail.com>
2020-09-06 20:01:06 -0300

    Remove unnecessary target

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -1,10 +1,5 @@
 import theme from '@nuxt/content-theme-docs'
 
 export default theme({
-  target: 'static',
-  router: {
-    base: '/vishnucss.github.io/vishnu/'
-  },
-  loading: { color: '#8549b9' },
-  liveEdit: false
+  loading: { color: '#8549b9' }
 })
