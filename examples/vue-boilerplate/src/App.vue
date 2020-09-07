7623c5017f0669f6ac2886c7653a314c8a7f20bd
Allan Alexandre <alexandesigner@gmail.com>
2020-09-06 21:43:20 -0300

    (examples): Create new examples and updates
    - Create vue boilerplate example
    - Create react boilerplate example
    - Update current examples to new brand

---
+++ examples/vue-boilerplate/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app" class="flex center full-screen">
+    <img alt="Vue logo" src="./assets/logo.png" height="165">
+    <h1>Vishnu</h1>
+    <a
+      href="https://vishnucss.github.io/vishnu"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Learn Vishnu
+    </a>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+#app {
+  font-family: Inter, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+#app h1 {
+  font-size: 62px;
+  font-weight: 600;
+  margin-top: 10px;
+}
+</style>
