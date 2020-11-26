b933c55d6f17c74c78db6c9a7d58336e22bd6336
Allan <alexandesigner@gmail.com>
2020-11-25 21:49:56 -0300

    add svelte example

---
+++ examples/svelte-boilerplate/src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+	export let name;
+</script>
+
+<main class="flex center full-screen text-center">
+ <img alt="Vue logo" src="/logo.png" height="165">
+  <h1>{name}</h1>
+  <a
+    href="https://vishnucss.github.io/vishnu"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Learn Vishnu
+  </a>
+</main>
+
+<style>
+  a {
+    color: var(--primary-color);
+  }
+</style>
